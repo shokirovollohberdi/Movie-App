e451291414b05bc490d75cb7b28ef2f6afc77472
Shokirov <ollohberdiinfo@gmail.com>
2021-08-09 22:36:03 +0300

    Movie app

--- app/src/main/java/RvAdapters/RvAdapters.kt
+++ app/src/main/java/RvAdapters/RvAdapters.kt
@@ -8,36 +8,39 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.AboutActivty
 import com.example.movieapp.Edit_Activity
 import com.example.movieapp.R
-import kotlinx.android.synthetic.main.activity_add_movio.view.*
 import kotlinx.android.synthetic.main.item_rv.view.*
 import java.lang.Exception
 
-class RvAdapters(var context: Context,val list: ArrayList<RvModels>) : RecyclerView.Adapter<RvAdapters.Vh>() {
+class RvAdapters(var context: Context, val list: ArrayList<RvModels>) : RecyclerView.Adapter<RvAdapters.Vh>() {
     inner class Vh(var itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun onBind(rvModels: RvModels,position: Int){
+        fun onBind(rvModels: RvModels, position: Int) {
             itemView.item_name.text = rvModels.name
             itemView.item_actor.text = rvModels.actor
             itemView.item_date.text = rvModels.date
+            var anim1:Animation = AnimationUtils.loadAnimation(context,R.anim.anim1)
+            itemView.startAnimation(anim1)
 
 
             try {
                 itemView.setOnClickListener {
-                   Position.position = position
+                    Position.position = position
                     context.startActivity(Intent(context, AboutActivty::class.java))
                 }
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 Toast.makeText(context, "$e", Toast.LENGTH_SHORT).show()
             }
 
             itemView.item_edit.setOnClickListener {
-                val intent = Intent(context,Edit_Activity::class.java)
-                intent.putExtra("model",rvModels)
-                intent.putExtra("position",position)
+                val intent = Intent(context, Edit_Activity::class.java)
+                intent.putExtra("model", rvModels)
+                intent.putExtra("position", position)
                 context.startActivity(intent)
 
             }
@@ -54,12 +57,12 @@ class RvAdapters(var context: Context,val list: ArrayList<RvModels>) : RecyclerV
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvAdapters.Vh {
 
         return Vh(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_rv,parent,false)
+                LayoutInflater.from(parent.context).inflate(R.layout.item_rv, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: RvAdapters.Vh, position: Int) {
-        holder.onBind(list[position],position)
+        holder.onBind(list[position], position)
     }
 
     override fun getItemCount(): Int = list.size
